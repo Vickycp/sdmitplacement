5795024bb92530eae5b2f310414645270483e082
vickycp <vickycp417@gmail.com>
2020-10-08 00:29:12 +0530

    main initilisation with fire base core

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,10 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:sdmitplacement/view/auth/authfrontend/signinPage.dart';
 
-void main() {
+Future<void> main() async{
+   WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   runApp(MyApp());
 }
 
