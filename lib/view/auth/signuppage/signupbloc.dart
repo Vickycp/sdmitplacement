1d0c58bd589cd6d26e84214bdce2aa4299e56a2e
vickycp <vickycp417@gmail.com>
2020-10-13 16:00:32 +0530

    signup and signupbloc competed success

--- lib/view/auth/signuppage/signupbloc.dart
+++ lib/view/auth/signuppage/signupbloc.dart
@@ -1,18 +1,172 @@
+import 'dart:async';
+
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:google_sign_in/google_sign_in.dart';
+import 'package:rxdart/rxdart.dart';
 import 'package:rxdart/subjects.dart';
+import 'package:sdmitplacement/view/auth/bloc/authbloc_bloc.dart';
 
 class SignupBloc {
-   final  _username = BehaviorSubject<String>();
+  FirebaseAuth _auth = FirebaseAuth.instance;
+  FirebaseFirestore _firestore = FirebaseFirestore.instance;
+  final GoogleSignIn _googleSignIn = GoogleSignIn();
+  final _username = BehaviorSubject<String>();
   final _usn = BehaviorSubject<String>();
-  final  _email = BehaviorSubject<String>();
-  final  _password = BehaviorSubject<String>();
-  final  _confirmpassword = BehaviorSubject<String>();
+  final _email = BehaviorSubject<String>();
+  final _password = BehaviorSubject<String>();
+  final _confirmpassword = BehaviorSubject<String>();
+  final _loading = PublishSubject<bool>();
+  final _signupsucess = BehaviorSubject<String>();
 
-  Stream<String> get usernamestream => _username.stream;
-  Stream<String> get usnstream => _username.stream;
-  // Stream<String> get  => _username.stream;
-  // Stream<String> get usernamestream => _username.stream;
-  // Stream<String> get usernamestream => _username.stream;
-  
+  final _googleloading = PublishSubject<bool>();
+
+  Stream<String> get usernamestream =>
+      _username.stream.transform(usertransform);
+  Stream<String> get usnstream => _usn.stream.transform(usntransform);
+  Stream<String> get emailstream => _email.stream.transform(emailtransform);
+  Stream<String> get passwordstream =>
+      _password.stream.transform(passwordtransform);
+  Stream<String> get confirmpassword =>
+      _confirmpassword.stream.transform(confirmpasswordtransform);
+  Stream<bool> get loading => _loading.stream;
+  Stream<String> get signupsuccess => _signupsucess.stream;
+  Stream<bool> get googleloading => _googleloading.stream;
+
+  Function(String) get usernamesink => _username.sink.add;
+  Function(String) get usnsink => _usn.sink.add;
+  Function(String) get emailsink => _email.sink.add;
+  Function(String) get passwordsink => _password.sink.add;
+  Sink get confirmpasswordsink => _confirmpassword.sink;
+
+  Stream<bool> get enable => Rx.combineLatest5(usernamestream, usnstream,
+      emailstream, passwordstream, confirmpassword, (a, b, c, d, e) => true);
+
+  matchpassword(String val) {
+    if (val == _password.value) {
+      confirmpasswordsink.add("match");
+    } else {
+      confirmpasswordsink.add("not match");
+    }
+  }
+
+  signup(BuildContext context) async {
+    _loading.sink.add(true);
+    await _auth
+        .createUserWithEmailAndPassword(
+            email: _email.value, password: _password.value)
+        .then((value) async {
+      _signupsucess.sink.add((value.user).email);
+
+      await _firestore.collection('studentcredential').doc().set({
+        'email': _email.value,
+        'password': _password.value,
+        'username': _username.value,
+        'usn': _usn.value
+      });
+      BlocProvider.of<AuthblocBloc>(context).add(Userauthsuccess());
+    }).catchError((error) {
+      _signupsucess.sink.addError(error.code);
+      _loading.sink.add(false);
+    });
+    _loading.sink.add(false);
+  }
+
+  googlesignup(BuildContext context) async {
+    _googleloading.sink.add(true);
+    final GoogleSignInAccount googleUser = await _googleSignIn.signIn();
+    final GoogleSignInAuthentication googleAuth =
+        await googleUser.authentication;
+
+    final AuthCredential credential = GoogleAuthProvider.credential(
+      accessToken: googleAuth.accessToken,
+      idToken: googleAuth.idToken,
+    );
+    await _auth
+        .signInWithCredential(credential)
+        .then((value)async {
+
+         _signupsucess.sink.add((value.user).email);
+      await _firestore.collection('studentcredential').doc().set({
+        'email': (value.user).email,
+        'password':null,
+        'username':(value.user).displayName,
+        'usn': (value.user).displayName
+      });
+       BlocProvider.of<AuthblocBloc>(context).add(Userauthsuccess());
+
+        })
+        .catchError((error) {
+  _signupsucess.sink.addError(error.code);
+      _googleloading.sink.add(false);
+
+        });
+    _googleloading.sink.add(false);
+  }
+
+  final usertransform = StreamTransformer<String, String>.fromHandlers(
+    handleData: (data, sink) {
+      if (data.length > 3) {
+        sink.add(data);
+      } else {
+        sink.addError("username should be greater than 3 letters");
+      }
+    },
+  );
+  final usntransform = StreamTransformer<String, String>.fromHandlers(
+    handleData: (data, sink) {
+      bool usnvalidate(usn) {
+        String validusn = r'^[1-4][a-z,A-Z]{2}[0-9]{2}[a-z,A-Z]{2}[0-9]{3}$';
+        RegExp exp = RegExp(validusn);
+        return exp.hasMatch(usn);
+      }
+
+      if (usnvalidate(data)) {
+        sink.add(data);
+      } else {
+        sink.addError("usn not valid");
+      }
+    },
+  );
+  final emailtransform = StreamTransformer<String, String>.fromHandlers(
+    handleData: (data, sink) {
+      bool isEmail(String email) {
+        // print(email);
+        String p =
+            r'^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$';
+
+        RegExp regExp = new RegExp(p);
+
+        return regExp.hasMatch(email);
+      }
+
+      if (isEmail(data)) {
+        sink.add(data);
+      } else {
+        sink.addError("email not correct");
+      }
+    },
+  );
+  final passwordtransform = StreamTransformer<String, String>.fromHandlers(
+    handleData: (data, sink) {
+      if (data.length > 6) {
+        sink.add(data);
+      } else {
+        sink.addError("password greater then 6");
+      }
+    },
+  );
+  final confirmpasswordtransform =
+      StreamTransformer<String, String>.fromHandlers(
+    handleData: (data, sink) {
+      print(data);
+      if (data.length > 6) {
+        sink.add(data);
+      }
+    },
+  );
 
   void dispose() {
     _username.close();
@@ -20,5 +174,8 @@ class SignupBloc {
     _email.close();
     _password.close();
     _confirmpassword.close();
+    _loading.close();
+    _signupsucess.close();
+    _googleloading.close();
   }
 }
--- lib/view/auth/signuppage/signuppage.dart
+++ lib/view/auth/signuppage/signuppage.dart
@@ -4,6 +4,7 @@ import 'package:flutter_signin_button/button_view.dart';
 import 'package:flutter_signin_button/flutter_signin_button.dart';
 
 import 'package:sdmitplacement/view/auth/bloc/authbloc_bloc.dart';
+import 'package:sdmitplacement/view/auth/signuppage/signupbloc.dart';
 
 class SignupPage extends StatefulWidget {
   @override
@@ -11,88 +12,222 @@ class SignupPage extends StatefulWidget {
 }
 
 class _SignupPageState extends State<SignupPage> {
+  String password, error;
+  bool showpassword = true;
+
+  SignupBloc _signupBloc = SignupBloc();
 
   @override
   Widget build(BuildContext context) {
-
     return Scaffold(
       body: Container(
-       padding: EdgeInsets.only(top: 30),
+        padding: EdgeInsets.only(top: 20),
         decoration: BoxDecoration(
           gradient: LinearGradient(
               begin: Alignment.topRight,
               end: Alignment.bottomLeft,
               colors: [Colors.blueGrey, Colors.lightBlueAccent]),
         ),
-        child: ListView(
-                  children:[ Column(
-
-
+        child: ListView(children: [
+          Column(
             children: [
-
-               Padding(
-                  padding: const EdgeInsets.only(top: 20.0),
-                  //log  of the college
-                  child: Center(
-                    child: FlutterLogo(
-                      size: 100,
-                    ),
+              Padding(
+                padding: const EdgeInsets.only(top: 20.0),
+                //log  of the college
+                child: Center(
+                  child: FlutterLogo(
+                    size: 100,
                   ),
                 ),
-                SizedBox(
-                  height: 20,
-                ),
-                Padding(
-                padding: const EdgeInsets.only(left:10.0,right: 10.0),
-                child: TextField(
-                  onChanged: (value) {},
-                  decoration: InputDecoration(labelText: "username",labelStyle: TextStyle(color: Colors.black)),
-                ),
               ),
-               Padding(
-                padding: const EdgeInsets.only(left:10.0,right: 10.0),
-                child: TextField(
-                  onChanged: (value) {},
-                  decoration: InputDecoration(labelText: "usn",labelStyle: TextStyle(color: Colors.black)),
+              SizedBox(
+                height: 5,
+              ),
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: StreamBuilder(
+                  stream: _signupBloc.signupsuccess,
+                  builder: (BuildContext context, AsyncSnapshot snapshot) {
+                    if (snapshot.hasError) {
+                      return Container(
+                        color: Colors.red,
+                        alignment: Alignment.center,
+                        child: Text(
+                          snapshot.error,
+                          style: TextStyle(
+                              color: Colors.black, fontStyle: FontStyle.italic),
+                        ),
+                      );
+                    }
+                    if (snapshot.hasData) {
+                      return Container(
+                        color: Colors.greenAccent[400],
+                        alignment: Alignment.center,
+                        child: Text(snapshot.data,
+                            style: TextStyle(
+                                color: Colors.black,
+                                fontStyle: FontStyle.italic)),
+                      );
+                    }
+                    return Container(
+                      child: Text(""),
+                    );
+                  },
                 ),
               ),
               Padding(
-                padding: const EdgeInsets.only(left:10.0,right: 10.0),
-                child: TextField(
-                  onChanged: (value) {},
-                  decoration: InputDecoration(labelText: "email",labelStyle: TextStyle(color: Colors.black)),
-                ),
+                padding: const EdgeInsets.only(left: 10.0, right: 10.0),
+                child: StreamBuilder(
+                    stream: _signupBloc.usernamestream,
+                    builder: (context, snapshot) {
+                      return TextField(
+                        textInputAction: TextInputAction.next,
+                        onChanged: _signupBloc.usernamesink,
+                        decoration: InputDecoration(
+                            errorText: snapshot.error,
+                            labelText: "username",
+                            labelStyle: TextStyle(color: Colors.black)),
+                      );
+                    }),
               ),
               Padding(
-                padding: const EdgeInsets.only(left:10.0,right: 10.0),
-                child: TextField(
-                  onChanged: (value) {},
-                 decoration: InputDecoration(labelText: "password",labelStyle: TextStyle(color: Colors.black)),
-                ),
-              ), Padding(
-                padding: const EdgeInsets.only(left:10.0,right: 10.0),
-                child: TextField(
-                  onChanged: (value) {},
-                 decoration: InputDecoration(labelText: "confirm password",labelStyle: TextStyle(color: Colors.black)),
-                ),
+                padding: const EdgeInsets.only(left: 10.0, right: 10.0),
+                child: StreamBuilder(
+                    stream: _signupBloc.usnstream,
+                    builder: (context, snapshot) {
+                      return TextField(
+                        textInputAction: TextInputAction.next,
+                        onChanged: _signupBloc.usnsink,
+                        decoration: InputDecoration(
+                            errorText: snapshot.error,
+                            labelText: "usn",
+                            labelStyle: TextStyle(color: Colors.black)),
+                      );
+                    }),
               ),
               Padding(
-                padding: const EdgeInsets.only(left:.0,right: 10.0,top: 10.0),
-                child: GestureDetector(
-                  onTap: () {},
-                  child: Container(
-                    alignment: Alignment.center,
-                    height: 40,
-                    width: 150,
-                    decoration: BoxDecoration(
-                      color: Colors.white,
-                      borderRadius: BorderRadius.circular(10.0),
-                    ),
-                    child: Text("signup")),
-                ),
+                padding: const EdgeInsets.only(left: 10.0, right: 10.0),
+                child: StreamBuilder(
+                    stream: _signupBloc.emailstream,
+                    builder: (context, snapshot) {
+                      return TextField(
+                        textInputAction: TextInputAction.next,
+                        onChanged: _signupBloc.emailsink,
+                        decoration: InputDecoration(
+                            errorText: snapshot.error,
+                            labelText: "email",
+                            labelStyle: TextStyle(color: Colors.black)),
+                      );
+                    }),
               ),
               Padding(
-                padding: const EdgeInsets.only(left:10.0,right: 10.0,top: 5.0),
+                padding: const EdgeInsets.only(left: 10.0, right: 10.0),
+                child: StreamBuilder(
+                    stream: _signupBloc.passwordstream,
+                    builder: (context, snapshot) {
+                      password = snapshot.data;
+                      return TextField(
+                        obscureText: showpassword,
+                        textInputAction: TextInputAction.next,
+                        onChanged: _signupBloc.passwordsink,
+                        decoration: InputDecoration(
+                            suffixIcon: IconButton(
+                                icon: showpassword
+                                    ? Icon(
+                                        Icons.remove_red_eye,
+                                        color: Colors.black,
+                                      )
+                                    : Icon(
+                                        Icons.remove_red_eye_outlined,
+                                        color: Colors.white,
+                                      ),
+                                onPressed: () {
+                                  setState(() {
+                                    showpassword = !showpassword;
+                                  });
+                                }),
+                            errorText: snapshot.error,
+                            labelText: "password",
+                            labelStyle: TextStyle(color: Colors.black)),
+                      );
+                    }),
+              ),
+              Padding(
+                  padding: const EdgeInsets.only(left: 10.0, right: 10.0),
+                  child: StreamBuilder(
+                      stream: _signupBloc.confirmpassword,
+                      builder: (context, snapshot) {
+                        print(snapshot.data);
+                        return TextField(
+                          obscureText: true,
+                          textInputAction: TextInputAction.done,
+                          onChanged: (val) {
+                            if (val != password) {
+                              setState(() {
+                                error = "T";
+                              });
+                              _signupBloc.confirmpasswordsink.add(val);
+                            } else {
+                              setState(() {
+                                error = "F";
+                              });
+                            }
+                            _signupBloc.matchpassword(val);
+                          },
+                          decoration: InputDecoration(
+                              suffixIcon: (error == "F")
+                                  ? Icon(
+                                      Icons.check,
+                                      color: Colors.black,
+                                    )
+                                  : null,
+                              errorText: error == "T"
+                                  ? "password does not match"
+                                  : null,
+                              labelText: "confirm password",
+                              labelStyle: TextStyle(color: Colors.black)),
+                        );
+                      })),
+              Padding(
+                padding:
+                    const EdgeInsets.only(left: .0, right: 10.0, top: 10.0),
+                child: StreamBuilder(
+                    stream: _signupBloc.enable,
+                    initialData: false,
+                    builder: (context, snapshot) {
+                      return GestureDetector(
+                        onTap: (snapshot.data == true)
+                            ? () {
+                                _signupBloc.signup(context);
+                              }
+                            : null,
+                        child: Container(
+                            alignment: Alignment.center,
+                            height: 40,
+                            width: 150,
+                            decoration: BoxDecoration(
+                              color: (snapshot.data == true)
+                                  ? Colors.white
+                                  : Colors.grey[600],
+                              borderRadius: BorderRadius.circular(10.0),
+                            ),
+                            child: StreamBuilder(
+                                initialData: false,
+                                stream: _signupBloc.loading,
+                                builder: (context, snapshot) {
+                                  if (snapshot.data == true) {
+                                    return CircularProgressIndicator(
+                                      backgroundColor: Colors.red,
+                                    );
+                                  }
+                                  return Text("signup");
+                                })),
+                      );
+                    }),
+              ),
+              Padding(
+                padding:
+                    const EdgeInsets.only(left: 10.0, right: 10.0, top: 5.0),
                 child: Row(
                   crossAxisAlignment: CrossAxisAlignment.center,
                   mainAxisAlignment: MainAxisAlignment.center,
@@ -103,26 +238,44 @@ class _SignupPageState extends State<SignupPage> {
                     ),
                     GestureDetector(
                         onTap: () {
-                         BlocProvider.of<AuthblocBloc>(context).add(Sigininbutton());
+                          BlocProvider.of<AuthblocBloc>(context)
+                              .add(Sigininbutton());
                         },
                         child: Padding(
                           padding: const EdgeInsets.all(8.0),
-                          child: Text("Signin",style: TextStyle(color: Colors.black,fontSize: 15.0),),
+                          child: Text(
+                            "Signin",
+                            style:
+                                TextStyle(color: Colors.black, fontSize: 15.0),
+                          ),
                         )),
                   ],
                 ),
               ),
-              SignInButton(
-                        Buttons.Google,
-                        text: "Signup with Google",
-                        onPressed: () {
-                    
-                        },
-                      ),
-
+              StreamBuilder(
+                  stream: _signupBloc.googleloading,
+                  initialData: false,
+                  builder: (context, snapshot) {
+                    if (snapshot.data == true) {
+                      return Container(
+                        decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(10.0)),
+                        child: CircularProgressIndicator(
+                          backgroundColor: Colors.red,
+                        ),
+                      );
+                    }
+                    return SignInButton(
+                      Buttons.Google,
+                      text: "Signup with Google",
+                      onPressed: () {
+                        _signupBloc.googlesignup(context);
+                      },
+                    );
+                  }),
             ],
-          )]
-        ),
+          )
+        ]),
       ),
     );
   }
